6ddc83bf5e0692c1df18f7ad3121f322e7858ce6
ayussh007 <38324946+ayussh007@users.noreply.github.com>
2023-03-03 00:49:54 +0530

    Delete ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/frontend/src directory

--- ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/frontend/src/config.js
+++
@@ -1,2 +0,0 @@
-const endPoint="https://bd825f78-a249-4a37-807c-2e2382714755.mock.pstmn.io/v1/videos";
-export default endPoint;
--- ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/ayussh-arora-ME_BUILDOUT_XFLIX_REACT-master-frontend-src/frontend/src/constants/constants.js
+++
@@ -1,2 +0,0 @@
-export const AllGenreParameter = "All";
-export const ReleaseDate = "releaseDate";
